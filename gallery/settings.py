ab34b4c987cfd642b638bec68eac046266773184
Duncan-Kiragu <mathenge089@gmail.com>
2020-05-23 22:11:27 +0300

    making changes to mainsettings file

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'photos',
+    'bootstrap4',
+    'photos.apps.PhotosConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
